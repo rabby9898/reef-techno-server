72eb450d6708efdbc44506e05138c910a3029c72
rabby9898 <mdfajlerabby00@gmail.com>
2023-10-18 09:20:37 +0600

    Add product pages create operation added

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ app.use(express.json());
 // mongodb
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qczjssr.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,6 +28,15 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const productsCollection = client.db("productDB").collection("product");
+
+    // add product CREATE
+    app.post("/products", async (req, res) => {
+      const AllProducts = req.body;
+      const result = await productsCollection.insertOne(AllProducts);
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
